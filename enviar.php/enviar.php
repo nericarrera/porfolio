6267ede3d9f03b67c8158fd975b86d32c13abc3d
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-11-12 22:39:20 -0300

    ok

--- enviar.php/enviar.php
+++ enviar.php/enviar.php
@@ -1,36 +1,33 @@
 <?php
-
-$headers = "From: $nombre <$correo>\r\n";
-$headers .= "Reply-To: $correo\r\n";
-$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
-
-mail($destinatario, $asunto, $carta, $headers);
-
 // LLAMANDO A LOS CAMPOS
-
 $nombre = $_POST['nombre'];
 $telefono = $_POST['telefono'];
 $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
 // DATOS PARA EL CORREO
-
 $destinatario = "nericarrera@yahoo.com.ar";
 $asunto = "Contacto desde nuestra web";
 
+// CONSTRUYENDO EL MENSAJE
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
-$carta .= "telefono: $telefono \n";
+$carta .= "Teléfono: $telefono \n";
 $carta .= "Mensaje: $mensaje";
 
+// CABECERAS
+$headers = "From: $nombre <$correo>\r\n";
+$headers .= "Reply-To: $correo\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
 // ENVIANDO MENSAJE
-
-mail($destinatario, $asunto, $carta);
-header('Location:mensaje-de-envio.html');
-
-
-
-
+if (mail($destinatario, $asunto, $carta, $headers)) {
+    // Si el mensaje se envía correctamente, redirige al usuario a la página de confirmación
+    header('Location: mensaje-de-envio.html');
+    exit();
+} else {
+    echo "Error al enviar el mensaje. Inténtelo más tarde.";
+}
 ?>
 
 
